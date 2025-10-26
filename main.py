1c70e766fa7271129a0da9dbf301b6f5f6a322a1
Lebron057 <iagoamorim057@gmail.com>
2025-10-26 11:25:14 -0300

    Commit inicial do projeto converte_frame

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+import os
+
+def frames_de_video(entrada, caminho_saida):
+    entrada_capturada = cv2.VideoCapture(entrada)
+    cont = 0
+    
+    while entrada_capturada.isOpened():
+        retorno, frame = entrada_capturada.read()
+        
+        if retorno:
+            cv2.imwrite(os.path.join(caminho_saida, "%d.png") %cont, frame)
+            cont += 1
+            
+        else:
+            break
+        
+    cv2.destroyAllWindows()
+    entrada_capturada.release()
+    
+frames_de_video("video.mp4", "frames")
